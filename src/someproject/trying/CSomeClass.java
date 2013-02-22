f0bd86f5526342232f514a2032cdb931268ea842
wut <wut@wuttary.com>
2013-02-22 15:28:38 +0600

    ff

---
+++ src/someproject/trying/CSomeClass.java
@@ -0,0 +1,30 @@
+package someproject.trying;
+
+public class CSomeClass {
+	
+	int a = 0;
+	String b = null;
+	
+	CSomeClass() {
+		a = 0;
+		b = "";
+	}
+	
+	CSomeClass(int aa, String bb) {
+		if(bb == null) b = "";
+		else b = new String(bb);
+		
+		a = aa;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Try this!");
+		
+	}
+
+}
