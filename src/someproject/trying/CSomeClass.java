5ae0839a560e443b5f3e8376efc52709796395c6
wut <wut@wuttary.com>
2013-02-22 15:45:06 +0600

    dd

--- src/someproject/trying/CSomeClass.java
+++ src/someproject/trying/CSomeClass.java
@@ -23,7 +23,7 @@ public class CSomeClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Try this! And that");
+		System.out.println("Try this! And that gg");
 		
 	}
 
